b9646fb11e504bda98fcb9168925d4acbcc0300c
Denis <denisdmm@gmail.com>
2024-06-05 01:21:26 -0300

    adicionar o projeto webapp, criar a tela login, e a tela home.

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TopoComponent } from "./layout/topo/topo.component";
+import { RodapeComponent } from "./layout/rodape/rodape.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, TopoComponent, RodapeComponent]
 })
 export class AppComponent {
   title = 'webapp';
